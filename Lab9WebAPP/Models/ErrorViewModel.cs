f6b86bd81e41ed5e3d11306e7c13d1c1f37c52ad
Gabriel7721 <tribui1990@gmail.com>
2023-11-21 23:29:05 +0700

    ok

--- Lab9WebAPP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab9WebAPP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
